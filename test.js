1617474c9422689b92ede6977ab4b1f7112460b8
duckeydev <duckeydev@gmail.com>
2025-09-28 13:09:49 +0630

    Small update
    Signed-off-by: duckeydev <duckeydev@gmail.com>

--- test.js
+++
@@ -1,3 +0,0 @@
-(async () => {
-console.log(await Bun.password.verify("DuckeyDev$69", "$argon2id$v=19$m=65536,t=2,p=1$2OH8YWiZQHYLBQ5VTU3FKhSlrSfBytxeyb9b4Lcsx1M$Nwia1oIMNvRl4f9e9Phn3MlI/ECrS0lLFANccaZVU7w"))
-})()
