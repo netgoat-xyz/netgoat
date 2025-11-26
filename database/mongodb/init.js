0614fe43a44f348f4dc28f7568062549f231fe3d
duckeydev <duckeydev@gmail.com>
2025-11-26 20:31:31 +0630

    PATCH: MongoDB with Special characters not being URI incoded
    Signed-off-by: duckeydev <duckeydev@gmail.com>

--- database/mongodb/init.js
+++ database/mongodb/init.js
@@ -1,9 +1,25 @@
 import mongoose from "mongoose";
 
-mongoose.connect(process.env.mongodb).then(() => {
-    logger.success("Connected to MongoDB")
+function sanitizeMongoURI(uri) {
+    try {
+        const url = new URL(uri);
+        if (url.password) {
+            url.password = encodeURIComponent(url.password);
+        }
+
+        return url.toString();
+    } catch (err) {
+        console.error("Invalid MongoDB URI:", err);
+        return uri; 
+    }
+}
+
+const safeURI = sanitizeMongoURI(process.env.mongodb);
+
+mongoose.connect(safeURI).then(() => {
+    logger.success("Connected to MongoDB");
 }).catch((err) => {
-    logger.error(err)
-    logger.error("Crashing to prevent further damage")
-    process.exit(1)
-})
+    logger.error(err);
+    logger.error("Crashing to prevent further damage");
+    process.exit(1);
+});
