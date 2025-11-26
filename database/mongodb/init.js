2ce4c2b9847a8e8ac5ddf75397c162d093851289
duckeydev <duckeydev@gmail.com>
2025-11-26 20:44:35 +0630

    PATCH: Hope this fixes it
    Signed-off-by: duckeydev <duckeydev@gmail.com>

--- database/mongodb/init.js
+++ database/mongodb/init.js
@@ -1,25 +1,37 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
+import logger from "./logger"
 
-function sanitizeMongoURI(uri) {
-    try {
-        const url = new URL(uri);
-        if (url.password) {
-            url.password = encodeURIComponent(url.password);
-        }
+const sanitize = (uri) => {
+  const match = uri.match(/mongodb(?:\+srv)?:\/\/([^:]+):([^@]+)@(.+)/)
+  if (!match) return uri
 
-        return url.toString();
-    } catch (err) {
-        console.error("Invalid MongoDB URI:", err);
-        return uri; 
-    }
+  const user = match[1]
+  const pass = encodeURIComponent(match[2])
+  const rest = match[3]
+
+  return `mongodb://${user}:${pass}@${rest}`
 }
 
-const safeURI = sanitizeMongoURI(process.env.mongodb);
+const raw = process.env.MONGODB_URI || ""
+if (!raw) {
+  logger("error", "No MONGODB_URI provided")
+  process.exit(1)
+}
 
-mongoose.connect(safeURI).then(() => {
-    logger.success("Connected to MongoDB");
-}).catch((err) => {
-    logger.error(err);
-    logger.error("Crashing to prevent further damage");
-    process.exit(1);
-});
+const MONGODB_URI = sanitize(raw)
+
+let DB_READY = false
+
+await mongoose
+  .connect(MONGODB_URI, { serverSelectionTimeoutMS: 5000 })
+  .then(() => {
+    DB_READY = true
+    logger("success", `Mongo connected`)
+  })
+  .catch((e) => {
+    logger("error", e.message)
+    logger("error", "Crashing to prevent further damage")
+    process.exit(1)
+  })
+
+export default DB_READY
